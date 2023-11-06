61147368c08df29e3410c674bee23bee60d681bf
dow00n <148226231+dow00n@users.noreply.github.com>
2023-11-06 21:20:32 +0800

    Add files via upload

---
+++ StudiKasus1/Tabung.java
@@ -0,0 +1,11 @@
+package StudiKasus1;
+
+public class Tabung extends BangunRuang {
+    
+
+    public Tabung(double jariJari, double tinggiTabung) {
+        this.alas = jariJari * jariJari * 3.14;
+        this.tinggi = tinggiTabung;
+    }
+}
+
