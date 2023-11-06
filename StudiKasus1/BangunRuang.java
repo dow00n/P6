fd1b1137f6379b6245283c85e60d8405098ba759
dow00n <148226231+dow00n@users.noreply.github.com>
2023-11-06 21:18:32 +0800

    Add files via upload

---
+++ StudiKasus1/BangunRuang.java
@@ -0,0 +1,10 @@
+package StudiKasus1;
+
+public class BangunRuang {
+    protected double tinggi;
+    protected double alas;
+    
+    public double hitungVolume() {
+        return alas * tinggi;
+    }
+}
