2ab2738434ab622987a367e73d5c6cca3af49cf0
dow00n <148226231+dow00n@users.noreply.github.com>
2023-11-06 21:25:32 +0800

    Add files via upload

---
+++ StudiKasus2/Trapesium.java
@@ -0,0 +1,10 @@
+package StudiKasus2;
+
+public class Trapesium extends BangunDatar {
+    public Trapesium(double alas1, double alas2, double tinggi) {
+        this.alas = (alas1 + alas2) / 2;
+        this.tinggi = tinggi;
+    }
+
+}
+
