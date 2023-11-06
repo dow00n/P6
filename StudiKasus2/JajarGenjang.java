2eac20ff621d4ac909e71fc1eb1d61413e75536b
dow00n <148226231+dow00n@users.noreply.github.com>
2023-11-06 21:24:46 +0800

    Add files via upload

---
+++ StudiKasus2/JajarGenjang.java
@@ -0,0 +1,10 @@
+package StudiKasus2;
+
+
+public class JajarGenjang extends BangunDatar {
+    public JajarGenjang(double alas, double tinggi) {
+        this.alas = alas;
+        this.tinggi = tinggi;
+    }
+
+}
