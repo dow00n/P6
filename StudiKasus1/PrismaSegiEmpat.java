35c79150c38f2e94d3b2fff3a56632148674ccc5
dow00n <148226231+dow00n@users.noreply.github.com>
2023-11-06 21:20:12 +0800

    Add files via upload

---
+++ StudiKasus1/PrismaSegiEmpat.java
@@ -0,0 +1,12 @@
+package StudiKasus1;
+
+public class PrismaSegiEmpat extends BangunRuang {
+    
+    public PrismaSegiEmpat(double panjang, double lebar, double tinggiPrisma) {
+        this.alas = panjang * lebar;
+        this.tinggi = tinggiPrisma;
+    }
+
+    
+}
+
